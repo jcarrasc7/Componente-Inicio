31f1f7ef96c38e5e85152f4743ae48d76489c19d
jcarrascal <juandavidcarrascalberdugo@gmail.com>
2024-10-17 12:04:11 -0500

    Componente Inicio

--- src/app/utilidades/rutas/Ruteo.tsx
+++ src/app/utilidades/rutas/Ruteo.tsx
@@ -1,26 +1,27 @@
 import {Route, Routes} from "react-router-dom"
 
+import { Acerca } from "../../componentes/otros/Acerca"
+import { MenuCrear } from "../../componentes/menu/MenuCrear"
 import { MenuAdmin } from "../../componentes/menu/MenuAdmin"
 import { Inicio } from "../../componentes/contenedor/Inicio"
-import { MenuCrear } from "../../componentes/menu/MenuCrear"
 import { MenuListar } from "../../componentes/menu/MenuListar"
 import { MenuActualizar } from "../../componentes/menu/MenuActualizar"
-import { Acerca } from "../../componentes/otros/Acerca"
 import { NoEncontrado } from "../../componentes/contenedor/NoEncontrado"
 
 export const Ruteo = ()=>{
     return(
         <Routes>
+
             <Route path="/" element={<Inicio/>}/>
 
             <Route path="/mencre" element={<MenuCrear/>}/>
+
             <Route path="/menlis" element={<MenuListar/>}/>
             <Route path="/menadmin" element={<MenuAdmin/>}/>
             <Route path="/menactu/:codigo" element={<MenuActualizar/>}/>
-
             <Route path="/menuacer" element={<Acerca/>}/>
-            
             <Route path="*" element={<NoEncontrado/>}/>
+            
         </Routes>
     )
 }
